f45acb276deed2448e387cbdaed4e47a162271dd
Rohit-S-harma <rohitsharma290393@gmail.com>
2024-05-07 13:53:10 +0530

    metacharacters

---
+++ string/regs/metacharacters/usinGbackslash.js
@@ -0,0 +1,4 @@
+const mystring = "hello + world";
+const regExp = /\+/;
+const isMatch = regExp.test(mystring);
+console.log(isMatch);   
---
+++ string/regs/metacharacters/usingCharacterClass.js
@@ -0,0 +1,4 @@
+const mystring = "you have 2^2 marks";
+const regExp = /[\^]/;
+const isMatch = regExp.test(mystring);
+console.log(isMatch);
